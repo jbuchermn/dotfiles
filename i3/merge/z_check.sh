d9da65d7f2199a868dc0fa9256fd118364439945
Jonas Bucher <j.bucher.mn@gmail.com>
2018-03-10 12:06:02 +0100

    i3

---
+++ i3/merge/z_check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Check if backlight can be changed
+if [[ -z "$(command -v xbacklight)" ]]; then
+    echo "#######################################"
+    echo ""
+    echo "WARNING! Could not find xbacklight."
+    echo ""
+    echo "########################################"
+fi
+
+# Check if ALSA PulseAudio is installed
+if [[ -z "$(command -v alsamixer)" ]] || [[ -z "$(command -v pactl)" ]]; then
+    echo "#######################################"
+    echo ""
+    echo "WARNING! Could not find ALSA and PulseAudio."
+    echo ""
+    echo "########################################"
+fi
