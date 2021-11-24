4e51b4ed7499d0712c74c80a88653e08a8cfa6a6
Jonas Bucher <j.bucher.mn@gmail.com>
2021-11-24 21:17:01 +0100

    newm

--- newm/home/.config/newm/config.py
+++ newm/home/.config/newm/config.py
@@ -5,7 +5,15 @@ import psutil
 
 from pywm import (
     PYWM_MOD_LOGO,
-    # PYWM_MOD_ALT
+    PYWM_MOD_ALT,
+
+    PYWM_TRANSFORM_90,
+    PYWM_TRANSFORM_180,
+    PYWM_TRANSFORM_270,
+    PYWM_TRANSFORM_FLIPPED,
+    PYWM_TRANSFORM_FLIPPED_90,
+    PYWM_TRANSFORM_FLIPPED_180,
+    PYWM_TRANSFORM_FLIPPED_270,
 )
 
 from newm import (
@@ -18,13 +26,15 @@ OUTPUT_MANAGER = True
 def on_startup():
     os.system("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots")
     os.system("catapult &")
+    # os.system("waybar &")
 
 # v1
 # output_scale = 2.
 
 outputs = [
     { 'name': 'eDP-1', 'pos_x': 0, 'pos_y': 0, 'scale': 2560/1500},
-    { 'name': 'virt-1', 'pos_x': 1500,'pos_y': 0,'width': 640, 'height': 480, 'mHz': 30000, 'scale': 1., 'anim': False},
+    { 'name': 'virt-1', 'pos_x': 1500,'pos_y': 0,'width': 1280, 'height': 720,
+        'mHz': 30000, 'scale': 1., 'anim': False},
     { 'name': 'HDMI-A-2', 'width': 3840, 'height': 2160, 'mHz': 30000, 'scale': 2.}
 ]
 
@@ -44,6 +54,17 @@ pywm = {
     # 'round_scale': 2.
 }
 
+def should_float(view):
+    if view.app_id == "catapult":
+        return True, None, (0.5, 0.25)
+    if view.app_id == "pavucontrol":
+        return True, (300, 600), (0.12, 0.35)
+    # if view.app_id == "rofi":
+    #     return True, (800, 800), (0.5, 0.5)
+    if view.title is not None and view.title.strip() == "Firefox — Sharing Indicator":
+        return True, (100, 40), (0.5, 0.1)
+    return None
+
 view = {
     # See comments in view.py
     'xwayland_handle_scale_clientside': not OUTPUT_MANAGER,
@@ -51,6 +72,12 @@ view = {
     'padding': 8,
     'fullscreen_padding': 0,
     'send_fullscreen': False,
+
+    'should_float': should_float,
+    'floating_min_size': True,
+
+    # 'debug_scaling': True
+    'border_ws_switch': 100
 }
 
 swipe_zoom = {
@@ -60,7 +87,10 @@ swipe_zoom = {
 
 
 mod = PYWM_MOD_LOGO
-wallpaper = '/home/jonas/wallpaper-4.jpg'
+background = {
+    'path': '/home/jonas/wallpaper-3.jpg',
+    'time_scale': 0.125,
+}
 
 anim_time = .25
 blend_time = .5
@@ -82,10 +112,14 @@ key_bindings = lambda layout: [
     ("M-C-k", lambda: layout.resize_focused_view(0, -1)),
     ("M-C-l", lambda: layout.resize_focused_view(1, 0)),
 
+    ("M-v", lambda: layout.toggle_focused_view_floating()),
+    ("M-w", lambda: layout.change_focused_view_workspace()),
+    ("M-S", lambda: os.system("grim -g \"$(slurp)\" &")),
+
     ("M-Return", lambda: os.system("alacritty &")),
     ("M-e", lambda: os.system("emacsclient -c -a \"emacs\" &")),
-    # ("M-c", lambda: os.system("chromium --enable-features=UseOzonePlatform --ozone-platform=wayland &")),
-    ("M-c", lambda: os.system("MOZ_ENABLE_WAYLAND=1 firefox &")),
+    ("M-c", lambda: os.system("chromium --enable-features=UseOzonePlatform --ozone-platform=wayland &")),
+    # ("M-c", lambda: os.system("MOZ_ENABLE_WAYLAND=1 firefox &")),
     ("M-q", lambda: layout.close_view()),
 
     ("M-p", lambda: layout.ensure_locked(dim=True)),
@@ -93,10 +127,10 @@ key_bindings = lambda layout: [
     ("M-C", lambda: layout.update_config()),
 
     ("M-r", lambda: os.system("catapult &")),
-    ("M-a", lambda: layout.enter_launcher_overlay()),
+    ("M-a", lambda: os.system("rofi -show run &")),
     ("M-f", lambda: layout.toggle_fullscreen()),
 
-    ("ModPress", lambda: layout.toggle_overview()),
+    ("ModPress", lambda: layout.toggle_overview(only_active_workspace=True)),
 ]
 
 sys_backend_endpoints = [
